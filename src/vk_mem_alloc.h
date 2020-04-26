bdad97383f8e46f914d5abc907d55e5e20dfc67f
Marcell Kiss <marcellk.kiss@gmail.com>
2020-04-26 23:08:43 +0200

    add freeall to VmaBlockMetaData_Generic

--- src/vk_mem_alloc.h
+++ src/vk_mem_alloc.h
@@ -5869,6 +5869,7 @@ public:
 
     virtual void Free(const VmaAllocation allocation);
     virtual void FreeAtOffset(VkDeviceSize offset);
+    virtual void FreeAll();
 
     ////////////////////////////////////////////////////////////////////////////////
     // For defragmentation
@@ -8860,6 +8861,13 @@ void VmaBlockMetadata_Generic::FreeAtOffset(VkDeviceSize offset)
         }
     }
     VMA_ASSERT(0 && "Not found!");
+}
+
+void VmaBlockMetadata_Generic::FreeAll() {
+    m_Suballocations.clear();
+    m_Suballocations.push_back({});
+    m_FreeCount = 1;
+    VMA_HEAVY_ASSERT(Validate());
 }
 
 bool VmaBlockMetadata_Generic::ValidateFreeSuballocationList() const
